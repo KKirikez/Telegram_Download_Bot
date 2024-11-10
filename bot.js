0d3b954cf62afda12a40b8fab7b3536e9a185ba7
kkirikez <kostyakirikeza@gmail.com>
2024-11-10 01:14:04 -0700

    message parsing logic done

--- bot.js
+++ bot.js
@@ -1,14 +1,33 @@
-require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
+// .env configuration
+// Your API key goes in there))
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+//looks for URLs
+const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(\/[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
+
+
+// Bot message logic
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
   
     if (messageText === '/start') {
-      bot.sendMessage(chatId, 'Send me video links and I will download them!');
+      bot.sendMessage(chatId, 'Send me video links!');
+    } else if(urlRegex.test(messageText)){
+        bot.sendMessage(chatId, 'Valid URL')
+    } else {
+        bot.sendMessage(chatId, 'Error, invalid video URL');
     }
   });
+
+function downloadPipeline(url){
+    
+}
+
