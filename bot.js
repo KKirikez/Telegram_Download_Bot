88f833e31a02a3b1b62ac23f8dbdcbc05335f9fe
kkirikez <kostyakirikeza@gmail.com>
2024-11-10 02:49:03 -0700

    Downloading and deleting work

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@
 // Your API key goes in there))
 require('dotenv').config();
 
+process.env.NTBA_FIX_350 = 'true'; 
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.BOT_TOKEN;
@@ -21,41 +23,37 @@ const ytDlpWrap = new YTDlpWrap('./bin/binary');
 
 
 // Bot message logic
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
   
     if (messageText === '/start') {
-      bot.sendMessage(chatId, 'Send me video links!');
+        bot.sendMessage(chatId, 'Send me video links!');
     } else if(urlRegex.test(messageText)){
         bot.sendMessage(chatId, 'Valid URL detected, processing now!');
         
-        downloadPipeline(messageText);
-        
-        bot.sendVideo(chatId, './videos/output.mp4');
+        await downloadPipeline(messageText);
+
+        await bot.sendVideo(chatId, './videos/video.mp4');
         
         bot.sendMessage(chatId, 'Finished!');
         
-        deleteMedia(url);
+        deleteMedia('video');
     } else {
         bot.sendMessage(chatId, 'Error, invalid video URL.');
     }
   });
 
 async function downloadPipeline(url){
-    let metadata = await ytDlpWrap.getVideoInfo(
-        url
-    );
     let stdout = await ytDlpWrap.execPromise([
         url,
         '-f',
         'best',
         '-o',
-        `./videos/${metadata.title}.mp4`,
+        `./videos/video.mp4`,
     ]);
-    
 }
 
-async function deleteMedia(url){
-    fs.unlink(`./videos/${url}.mp4`)
+async function deleteMedia(id){
+    fs.unlinkSync(`./videos/${id}.mp4`);
 }
