b1748e7768d54a3c75ad40a5fcc929aa56fcd4f0
kkirikez <kostyakirikeza@gmail.com>
2024-11-10 01:51:16 -0700

    Downloading finished

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
 // .env configuration
 // Your API key goes in there))
-import dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
 
-
-import TelegramBot from 'node-telegram-bot-api';
+const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-//looks for URLs
+//URL regular expression
 const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(\/[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?$/;
+
+const YTDlpWrap = require('yt-dlp-wrap').default;
+const fs = require('fs');
+
+YTDlpWrap.downloadFromGithub(
+    './bin/binary'
+);
+
+const ytDlpWrap = new YTDlpWrap('./bin/binary');
 
 
 // Bot message logic
@@ -21,13 +28,34 @@ bot.on('message', (msg) => {
     if (messageText === '/start') {
       bot.sendMessage(chatId, 'Send me video links!');
     } else if(urlRegex.test(messageText)){
-        bot.sendMessage(chatId, 'Valid URL')
+        bot.sendMessage(chatId, 'Valid URL detected, processing now!');
+        
+        downloadPipeline(messageText);
+        
+        bot.sendVideo(chatId, './videos/output.mp4');
+        
+        bot.sendMessage(chatId, 'Finished!');
+        
+        deleteMedia(url);
     } else {
-        bot.sendMessage(chatId, 'Error, invalid video URL');
+        bot.sendMessage(chatId, 'Error, invalid video URL.');
     }
   });
 
-function downloadPipeline(url){
+async function downloadPipeline(url){
+    let metadata = await ytDlpWrap.getVideoInfo(
+        url
+    );
+    let stdout = await ytDlpWrap.execPromise([
+        url,
+        '-f',
+        'best',
+        '-o',
+        `./videos/${metadata.title}.mp4`,
+    ]);
     
 }
 
+async function deleteMedia(url){
+    fs.unlink(`./videos/${url}.mp4`)
+}
